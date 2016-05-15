b7021b900956027ecb5b4d727b576d5fc7f6877f
Jesper Dalsgaard <post@jdanet.dk>
2016-05-15 13:23:46 +0200

    Forside template rettet. Media queries rettet til bootstrap breakpoints

--- page.php
+++ page.php
@@ -1,15 +1,10 @@
-<?php
-
-// Template Name: Forside
-
-?>
-
 <?php get_header(); ?>
 
 <?php get_template_part( 'navigation', 'default' ); ?>
 
 <div class="container main-container">
   <div class="row">
+    <div class="col-lg-9">
 <?php
   if ( have_posts() ) :
     while ( have_posts() ) : the_post();
@@ -20,6 +15,10 @@
     ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'bootstrap-four' ); ?></p><?php
   endif;
 ?>
+    </div><!-- .col -->
+    <div class='col-lg-3'>
+      <?php dynamic_sidebar( 'Right Sidebar' ); ?>
+    </div><!-- .col -->
   </div><!-- .row -->
 </div><!-- .container.main-container -->
 
