9450d68a22f8ef93524b5ad353ccf2b07141dc5d
Jesper Dalsgaard <post@jdanet.dk>
2016-05-20 11:51:52 +0200

    jQuery added - main.js added

--- functions.php
+++ functions.php
@@ -65,3 +65,25 @@ return count( $comments_by_type['comment'] );
 return $count;
 }
 }
+
+/* Stylesheet + Javascript fil registreres */
+
+function ravfestival_theme_script_enqueue() {
+	
+	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/main.js', array(), '1.0', true);
+	
+}
+
+add_action('wp_enqueue_scripts', 'ravfestival_theme_script_enqueue');
+
+/* Tilføj jQuery bibliotek */
+
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+
+function my_jquery_enqueue() {
+   wp_deregister_script('jquery');
+   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", false, null);
+   wp_enqueue_script('jquery');
+}
+
+/* ------------------------------------------------ */
