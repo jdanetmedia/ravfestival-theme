8b97367cf404b0942b2d7b4c738c548c498050cd
Kadir <kadirduran91@gmail.com>
2016-05-23 07:40:47 +0100

    Flere ændringer til forsiden

--- page-forside.php
+++ page-forside.php
@@ -14,25 +14,34 @@
 	<div id="overlay">
 		<div id=main_cnt>
 			<div id="logo_cnt"></div>
-			<div id="headertext_cnt"></div>
+			<div id="headertext_cnt">Ravfestival starter om:</div>
 			<div id="countdown_cnt"></div>
 		</div>
 	</div>
 </div>
-<section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
-<section class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
-</section>
-</article>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
-<?php endwhile; endif; ?>
-</section>
+<div id="front_page_cnt2">
+	<div id="second_cnt">
+		<div id="second_left_cnt">
+			<section id="content" role="main">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="header">
+            <h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+            </header>
+            <section class="entry-content">
+            <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+            <?php the_content(); ?>
+            <div class="entry-links"><?php wp_link_pages(); ?></div>
+            </section>
+            </article>
+            <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+            <?php endwhile; endif; ?>
+            </section>
+		</div>
+		<div id="second_right_cnt">ss</div>
+	</div>
+</div>
+<div id="front_page_cnt3"></div>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
