2bc416af00a7f7308c4596f6dda3df71af93212b
Jesper Dalsgaard <post@jdanet.dk>
2016-05-24 11:06:09 +0200

    Mere tablet styling af blog side

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php get_header(); ?>
+<div class="custom_logo">
+	<a href="<?php echo site_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/ravlogo-flat6.png" /></a>
+</div>
 <section id="content" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
