bb0a37a893431ed648c326e91ecb7f5802a0aa51
Jesper Dalsgaard <post@jdanet.dk>
2016-05-24 11:08:32 +0200

    Encoding ændret på certifikat template

--- page-certifikat.php
+++ page-certifikat.php
@@ -34,14 +34,14 @@
 <div id="certifikat_third_cnt">
 	<div id="third_cnt">
 		<div id="drengkigger"></div>
-		<div id="third_text"><h1>Find dit rav</h1><p>Gå på ravjagt på stranden og find ravet med dit værktøj og eagle vision!</p></div>
+		<div id="third_text"><h1>Find dit rav</h1><p>GÃ¥ pÃ¥ ravjagt pÃ¥ stranden og find ravet med dit vÃ¦rktÃ¸j og eagle vision!</p></div>
 	</div>
 </div>
 <div id="certifikat_fourth_cnt">
 	<div id="slib_text" onmouseover="ravslibeffekt()" onmouseout="ravslibeffektOut()">
 
 	<div class="picture_cnt">
-		<div id="sliber_text_holder"<h1>Slib dit Rav</h1><p>Tag din ravklump med til en af de lokale slibere og slib dit rav så det skinner!</p></div>
+		<div id="sliber_text_holder"<h1>Slib dit Rav</h1><p>Tag din ravklump med til en af de lokale slibere og slib dit rav sÃ¥ det skinner!</p></div>
 		<div id="sliber"></div>
 		<div id="ravhaand"></div>
 		<div id="glans"></div>
@@ -52,7 +52,7 @@
 <div id="certifikat_fifth_cnt">
 	<div id="smykke_cnt">
 		<div id="smykke"></div>
-		<div id="smykke_text"><h1>Lav dit ravsmykke</h1><p>Hos en af vores mange workshop kan du lave en armbånd, halskæde eller ring!</p></div>
+		<div id="smykke_text"><h1>Lav dit ravsmykke</h1><p>Hos en af vores mange workshop kan du lave en armbÃ¥nd, halskÃ¦de eller ring!</p></div>
 		<div id="ravsmykkeglow"></div>
 	</div>
 </div>
