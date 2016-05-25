0c709595e67b8a58080ab82ebbe9d52a426599a9
Jesper Dalsgaard <post@jdanet.dk>
2016-05-25 15:22:21 +0200

    En helt masse!

--- page.php
+++ page.php
@@ -1,17 +1,18 @@
 <?php get_header(); ?>
+<div class="custom_logo">
+	<a href="<?php echo site_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/ravlogo-flat6.png" /></a>
+</div>
+<div class="index-header">
+	<h1 class="index-title"><?php the_title(); ?></h1>
+</div>
 <section id="content" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
 <section class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <?php the_content(); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
 </article>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
 <?php endwhile; endif; ?>
 </section>
 <?php get_sidebar(); ?>
