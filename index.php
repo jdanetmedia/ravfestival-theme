a71920d44e1e66296f9d67c860f830f447b81011
Jesper Dalsgaard <post@jdanet.dk>
2016-05-25 10:12:59 +0200

    Custom font 'installeret'. Overskrift tilføjet på nyhedsside

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 <div class="custom_logo">
 	<a href="<?php echo site_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/ravlogo-flat6.png" /></a>
 </div>
+<div class="index-header">
+	<h1 class="index-title">Seneste nyheder</h1>
+</div>
 <section id="content" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <header>
 <?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
 </header>
 </article>
-
 <?php comments_template(); ?>
 <?php endwhile; endif; ?>
 <?php get_template_part( 'nav', 'below' ); ?>
