1cfbe34f812f4992282996177cf3063c846f4c3f
Jesper Dalsgaard <post@jdanet.dk>
2016-05-26 21:38:04 +0200

    Flere fejlrettelser

--- index.php
+++ index.php
@@ -17,5 +17,8 @@
 <?php endwhile; endif; ?>
 <?php get_template_part( 'nav', 'below' ); ?>
 </section>
-<?php get_sidebar(); ?>
+<div id="nyhedsbrev_cnt">
+	<div class="paamind">Få en påmindelse før Ravfestival</div>
+	<?php echo do_shortcode('[contact-form-7 id="180" title="Nyhedsbrev"]'); ?>
+</div>
 <?php get_footer(); ?>
